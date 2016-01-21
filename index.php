0d78df7dda9d2bef98527f749820536f9431391f
rujimenez <ruben.jimenez@betspin.com>
2016-01-21 16:40:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rubenjimenez
+ * Date: 21/01/16
+ * Time: 16:08
+ */
+class info{
+
+    protected $getSize;
+    protected $getCountImg;
+    protected $url;
+
+
+    public function __construct(){
+
+
+    }
+
+    public function getAll(){
+        $this->url=$_GET['url'];
+        $ch = curl_init($this->url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+
+        $data = curl_exec($ch);
+        $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+        //curl_close($ch);
+        return $data;
+
+    }
+
+}
+
+$download=new info();
+
+echo $download->getAll();
+
+
