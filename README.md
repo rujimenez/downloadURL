89af7872ea8c598fc484eef2c36853aaa7db469f
rubenjimenez <ruben.jimenez@betspin.com>
2016-01-22 18:31:04 +0100

    commit done for all files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # downloadURL
 Donload URL parameters
+=======================
+parameters:
+
+	URL - Url to check
+	Type - 
+		1    Want to open a url given with header info, size and css,js,img info
+		2    call goole API to check speed of the website
+
+- to execute just open /downloadURL/index?url=xxxxxxxx,type=x
+
+- example: http://localhost/downloadURL/index.php?url=http://evaarroyo.360webstudio.com/&type=1
+	   http://localhost/downloadURL/index.php?url=http://evaarroyo.360webstudio.com/&type=2
--- index.php
+++ index.php
@@ -2,43 +2,27 @@
 /**
  * Created by PhpStorm.
  * User: rubenjimenez
- * Date: 21/01/16
- * Time: 16:08
+ * Date: 22/01/16
+ * Time: 14:08
  */
-class info{
-
-    protected $getSize;
-    protected $getCountImg;
-    protected $url;
 
 
-    public function __construct(){
+require_once 'app.php';
 
-
-    }
-
-    public function getAll(){
-        $this->url=$_GET['url'];
-        $ch = curl_init($this->url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, TRUE);
-        curl_setopt($ch, CURLOPT_NOBODY, TRUE);
-
-        $data = curl_exec($ch);
-        $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-
-        curl_close($ch);
-        return 'Size : '.$size . '<br>DataHeaders : ' . $data;
+app::init();
 
 
 
-    }
 
-}
 
-$download=new info();
 
-echo $download->getAll();
+
+
+
+
+
+
+
+
 
 
